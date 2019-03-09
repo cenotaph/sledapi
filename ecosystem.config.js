169ac1168c710ada8d5a14aa3c0766019c2e0e08
John W. Fail <john@pixelache.ac>
2019-03-10 01:20:26 +0200

    change npm to yarn

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:cenotaph/sledapi.git',
       path : '/var/www/sled/api',
-      'post-deploy' : 'ln -sf /var/www/sled/api/shared/data /var/www/sled/api/current/data && cp /var/www/sled/api/shared/.env.production /var/www/sled/api/current && npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'ln -sf /var/www/sled/api/shared/data /var/www/sled/api/current/data && cp /var/www/sled/api/shared/.env.production /var/www/sled/api/current && yarn install && pm2 reload ecosystem.config.js --env production'
     }
   }
 };
